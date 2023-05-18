c45cce9ac300f1404b266524b114e1307586e3bf
Thai Pham Ngoc Tuong <thaiphamngoctuong@gmail.com>
2023-05-18 12:52:31 +0700

    commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,26 @@
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "../components";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return "Loading..."
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+      sx={{ ml: 2 }}
+    >
+      {videos.map((item, id) => {
+        return (
+          <Box key={id}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default Videos;
